23e2a4bbde6a57c2e9b7a23835f3b6e0fc8506f5
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-04-15 00:49:58 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SplitSection from './components/SplitSection'
+
+const App = () => {
+  return (
+   <div className='w-full h-screen'>
+     <SplitSection />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/SplitSection.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useState } from "react"
+import {
+	ContextMenu,
+	ContextMenuContent,
+	ContextMenuItem,
+	ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
+type Direction = "horizontal" | "vertical"
+
+const SplitSection = ({
+	sd,
+	defaultColor,
+}: { sd?: (val: Direction | null) => void; defaultColor?: string }) => {
+	const [direction, setDirection] = useState<Direction | null>(null)
+	const [color, setColor] = useState<string>(defaultColor ?? "#fff")
+
+	return (
+		<ContextMenu>
+			<ContextMenuTrigger asChild>
+				{direction ? (
+					<ResizablePanelGroup direction={direction}>
+						<ResizablePanel>
+							<SplitSection defaultColor={color} sd={setDirection} />
+						</ResizablePanel>
+						<ResizableHandle withHandle />
+						<ResizablePanel>
+							<SplitSection defaultColor={color} sd={setDirection} />
+						</ResizablePanel>
+					</ResizablePanelGroup>
+				) : (
+					<div className="w-full h-full grid place-items-center relative p-1 ">
+						<div
+							className="w-full h-full rounded-md"
+							style={{ background: color }}
+						></div>
+						<input
+							type="color"
+							className="absolute top-5 right-5"
+							defaultValue={color}
+							onChange={(e) => setColor(e.target.value)}
+						/>
+					</div>
+				)}
+			</ContextMenuTrigger>
+			<ContextMenuContent>
+				<ContextMenuItem onSelect={() => setDirection("horizontal")}>
+					Split Horizontal
+				</ContextMenuItem>
+				<ContextMenuItem onSelect={() => setDirection("vertical")}>
+					Split Vertical
+				</ContextMenuItem>
+                {sd && (
+                    <ContextMenuItem onSelect={() => sd(null)}>Remove</ContextMenuItem>
+                )}
+			</ContextMenuContent>
+		</ContextMenu>
+	)
+}
+
+export default SplitSection
